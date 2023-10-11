ddda1992686f1639dc2e205299e17ddb826ea7ea
cyanYellow <williecyanyellow@gmail.com>
2023-10-11 12:52:25 -0600

    Initial Commit

---
+++ Assignment7_Mapkit/Assignment7_MapkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment7_MapkitApp.swift
+//  Assignment7_Mapkit
+//
+//  Created by Willie Green on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment7_MapkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
